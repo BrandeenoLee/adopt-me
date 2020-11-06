729e1ab6ce0c3fb0f7487da36aeefd6a8d4ee01c
BrandeenoLee <47995527+BrandeenoLee@users.noreply.github.com>
2020-11-06 12:53:14 -0600

    Create App.js

---
+++ App.js
@@ -0,0 +1,25 @@
+const Pet = () => {
+    return React.createElement("div", {}, [
+        React.createElement("h1", {}, "Scout"),
+        React.createElement("h2", {}, "Dog"),
+        React.createElement("h2"s, {}, "Cow"),
+    ]);
+};
+
+const App = () => {
+    return React.createElement(
+        "div",
+        { id: "something-important" },
+        [
+            React.createElement("h1", {}, "Adopt Me!"),
+            React.createElement(Pet),
+            React.createElement(Pet),
+            React.createElement(Pet)
+        ]
+     );
+};
+
+ReactDOM.render(
+    React.createElement(App),
+    document.getElementById("root")
+);
